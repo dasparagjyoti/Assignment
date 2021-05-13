113f7c7a89ae6921518e1ae15884f911e5484f71
P.J.DAS@21 <paragjyotidas21@gmail.com>
2021-05-13 23:28:37 +0530

    Update 2.ReverseArray(Recursion).cpp

--- DSA with C++/2.ReverseArray(Recursion).cpp
+++ DSA with C++/2.ReverseArray(Recursion).cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 void reverseArray(int arr[],int start,int end)
 {
-	if (start >= end)
+	if (start >= end)  //base condition
 	return;
 
     int temp = arr[start];
     arr[start] = arr[end];
     arr[end] = temp;
 
-  reverseArray(arr, start+1,end-1);
+  reverseArray(arr, start+1,end-1); //function call
 }
 
 void printArray(int arr[],int size)
